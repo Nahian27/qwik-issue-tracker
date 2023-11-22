692d3b72d59eef1d34cea1e68d5192f1407ba8cc
Al-Nahian Pulok <alnpulok27@gmail.com>
2023-11-22 21:31:49 +0600

    prisma edge fix

--- src/libs/PrismaClient.ts
+++ src/libs/PrismaClient.ts
@@ -1,15 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client/edge";
 
 const prismaClientSingleton = () => new PrismaClient();
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
+  prisma: PrismaClientSingleton | undefined;
 };
 
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
